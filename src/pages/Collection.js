9b144415e9f13ffac88c2dcd85fa8760fe501a68
Brandon LEININGER <brandonleininger@MacBook-Pro-de-Brandon.local>
2020-12-19 00:00:03 +0100

    WIP collection page

--- src/pages/Collection.js
+++ src/pages/Collection.js
@@ -1,22 +1,35 @@
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { ApplicationContext } from "../domain/application.store";
+import { fetchPictureById } from "../domain/picture/picture.actions";
 
 export default function Collection() {
   const { state, dispatch } = useContext(ApplicationContext);
 
-  if (state.user) console.log(state.user.pictures_collection);
+  let fetchedPics = []
 
+    if (state.user) {
+      state.user.pictures_collection.map(picture => {
+        fetch(`/api/pictures/${picture.picsum_id}`)
+            .then((res) => res.json())
+            .then((res) => {
+              console.log(res)
+              fetchedPics.push(res)
+            });
+
+      })
+
+      console.log(state.pictures)
+      console.log(fetchedPics)
+    }
+
+  if (!state.user) return null
   return (
     <>
       <h1 className="title">My Collection</h1>
 
-      {state.user && (
-        <>
-          {state.user.pictures_collection.map((picture) => (
-            <p>Pic</p>
-          ))}
-        </>
-      )}
+      {state.user.pictures_collection.map((picture) => (
+        <p key={picture._id}>{picture.picsum_id}</p>
+      ))}
     </>
   );
 }
