a0800223c666cd9c7063b8ff1abb266273d18c6b
Brandon LEININGER <brandonleininger@MacBook-Pro-de-Brandon.local>
2020-12-18 10:11:10 +0100

    Logout v2

--- src/domain/authentication/authentication.service.js
+++ src/domain/authentication/authentication.service.js
@@ -38,8 +38,23 @@ export function login(data) {
         .then(res => res.json())
 }
 
-export function logout() {
-    document.cookie = "auth=; expires= Thu, 01 Jan 1970 00:00:00 UTC";
+export function logout(data) {
+    return fetch(`/api/logout/`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(async res => {
+        if (res.status !== 200 && res.status !== 201) {
+            const { message } = await res.json()
+            throw new Error(message)
+        }
+        return res
+    })
+    .then(res => res.json())
+    // document.cookie = "auth=; expires= Thu, 01 Jan 1970 00:00:00 UTC";
 }
 
 export function getMe() {
